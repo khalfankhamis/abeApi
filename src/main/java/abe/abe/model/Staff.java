c4d8f72dd8c35819cb1f031acc7da5e7377edb3d
khalfankhamis <dukaniabe@gmail.com>
2022-07-08 19:19:33 +0300

    second

---
+++ src/main/java/abe/abe/model/Staff.java
@@ -0,0 +1,24 @@
+package abe.abe.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Staff {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+}
